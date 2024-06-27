d7fb54d1a5c9b4e4d00cb89df7083e7a4417d1ee
DIFFFFFFT <672228275@qq.com>
2024-06-27 19:48:24 +0800

    first commit

---
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -0,0 +1,16 @@
+package cn.iocoder.yudao.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${yudao.info.base-package}
+@SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
+public class YudaoServerApplication {
+
+    // 整个服务的启动入口
+    public static void main(String[] args) {
+        SpringApplication.run(YudaoServerApplication.class, args);
+    }
+
+}
